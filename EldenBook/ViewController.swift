ade12534046809aee617999cdd162aa55c140a04
dev_alvin <alvinmatthew12@gmail.com>
2025-03-22 16:34:46 +0700

    Initial Commit

---
+++ EldenBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EldenBook
+//
+//  Created by Alvin Matthew Pratama on 22/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ EldenBookTests/EldenBookTests.swift
@@ -0,0 +1,17 @@
+//
+//  EldenBookTests.swift
+//  EldenBookTests
+//
+//  Created by Alvin Matthew Pratama on 22/03/25.
+//
+
+import Testing
+@testable import EldenBook
+
+struct EldenBookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
